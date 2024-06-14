e9dfaa5cecc1c60ca77f92abf3f71c1d30fc742a
reluck <lukas.reif@rdf.nuernberg.de>
2024-06-14 09:27:56 +0200

    implemented saving of multiple scores

--- GuessingGame_reluuk.py
+++ GuessingGame_reluuk.py
@@ -1,7 +1,9 @@
 import random
-def save_score(score):
-    x = open("HighscoreGuessingGame.txt", "w")
-    x.write(f"Highscore: {score}")
+x = open("HighscoreGuessingGame.txt", "w")
+def save_score(score, playername):
+    with open("HighscoreGuessingGame.txt", "a") as x:
+        x.write(f"player: {playername}\nscore: {score}\n")
+        x.close()
 def play():
     random_num = random.randint(1,100)
     print(f"DEBUG: {random_num}")
@@ -10,44 +12,45 @@ def play():
 
     while score < 10 and win == False:
         score += 1 # Kurzform für score = score + 1
-        geraten_num = input(f"({score})Rate eine ganze Zahl von 1 bis 100:")
+        geraten_num = input(f"({score})Guess a number from 1 to 100:")
         try:
             geraten_num = int(geraten_num)
             if geraten_num < 1 or geraten_num > 100:
                 raise ValueError # raise erzwingt eine Exception
         except ValueError:
-            print("Nur ganze Zahlen von 1 bis 100 sind gültig!")
+            print("Only whole numbers from 1 to 100 are valid!")
             continue # springt zum nächsten Schleifendurchlauf
         if geraten_num == random_num and score == 1:
-            print(f"Richtig geraten! Zufallszahl = {random_num}")
-            print("Perfekt! Highscore = 1.")
+            print(f"You guessed on the first try! wanted number = {random_num}")
+            print("Highscore = 1.")
             win = True
         elif geraten_num == random_num:
-            print(f"Richtig geraten! Zufallszahl = {random_num}")
+            print(f"You guessed right! wanted number = {random_num}")
             print(f"Score = {score}.")
             win = True
         elif geraten_num > random_num:
-            print(f"Gesuchte Zahl ist kleiner.")
+            print(f"The wanted number is smaller.")
             erraten_num = geraten_num
         elif score == 10:
-            print(f"Zu viele Versuche! ({score})")
+            print(f"Too many attempts ({score})")
             exit()
         else:
-            print(f"Gesuchte Zahl ist größer.")
+            print(f"The wanted number is bigger.")
             erraten_num = geraten_num
     if score == 10:
                 print(f"Zu viele Versuche! ({score})")
     return score
 while True:
-    save_score(play())
+    playername = input("input your playername: "
+    save_score(play(),playername))
     correctUserInput = False
     while not correctUserInput:
-        Continue = input("Nochmal spielen? [j/n]")
+        Continue = input("play again? [y/n]")
         #if Continue == "n":
         if "n" == Continue:
             exit()
-        elif Continue == "j":
+        elif Continue == "y":
             #break
             correctUserInput = True
         else:
-            print("Falsche Eingabe")
+            print("wrong input")
