fd8468510e571a03295e59eff9ef754e3546adb4
reluck <lukas.reif@rdf.nuernberg.de>
2024-06-14 10:24:05 +0200

    implemented max. attempts removed old code changed input text lang. to english

--- GuessingGame_reluuk.py
+++ GuessingGame_reluuk.py
@@ -1,17 +1,20 @@
-import random
-x = open("HighscoreGuessingGame.txt", "w")
-def save_score(score, playername):
+def save_score(score, playername, attempts):
+    score_highscore = ""
+    if score == 1:
+        score_highscore = "Highscore:"
+    else:
+        score_highscore = "Score:"
     with open("HighscoreGuessingGame.txt", "a") as x:
-        x.write(f"player: {playername}\nscore: {score}\n")
+        x.write(f"Player: {playername}\n{score_highscore} {score}/{attempts}\n")
         x.close()
-def play():
+def play(versuche):
     random_num = random.randint(1,100)
-    # print(f"DEBUG: {random_num}")
+    print(f"DEBUG: {random_num}")
     win = False
     score = 0
-    while score < 10 and win == False:
+    while score < versuche and win == False:
         score += 1 # Kurzform für score = score + 1
-        geraten_num = input(f"({score})Guess a number from 1 to 100:")
+        geraten_num = input(f"({score})Guess a number from 1 to 100 (max. {versuche} attempts):")
         try:
             geraten_num = int(geraten_num)
             if geraten_num < 1 or geraten_num > 100:
@@ -20,36 +23,39 @@ def play():
             print("Only whole numbers from 1 to 100 are valid!")
             continue # springt zum nächsten Schleifendurchlauf
         if geraten_num == random_num and score == 1:
-            print(f"You guessed on the first try! wanted number = {random_num}")
+            print(f"You guessed on the first try! Wanted number was {random_num}.")
             print("Highscore = 1.")
             win = True
         elif geraten_num == random_num:
-            print(f"You guessed right! wanted number = {random_num}")
+            print(f"You guessed right! Wanted number was {random_num}.")
             print(f"Score = {score}.")
             win = True
         elif geraten_num > random_num:
             print(f"The wanted number is smaller.")
-            erraten_num = geraten_num
-        elif score == 10:
-            print(f"Too many attempts ({score})")
-            exit()
         else:
             print(f"The wanted number is bigger.")
-            erraten_num = geraten_num
-    if score == 10:
-                print(f"Zu viele Versuche! ({score})")
+    if score == versuche:
+        print(f"Too many attempts! ({score})")
     return score
+import random
+x = open("HighscoreGuessingGame.txt", "w")
 while True:
-    playername = input("input your playername: ")
-    save_score(play(),playername)
+    playername = input("Input your playername: ")
+    versuche = input("Number of attempts: ")
+    try:
+        versuche = int(versuche)
+        if versuche < 0:
+            raise ValueError  # raise erzwingt eine Exception
+    except ValueError:
+        print("Wrong Input!")
+        continue  # springt zum nächsten Schleifendurchlauf
+    save_score(play(versuche), playername, versuche)
     correctUserInput = False
     while not correctUserInput:
         Continue = input("play again? [y/n]")
-        #if Continue == "n":
         if "n" == Continue:
             exit()
         elif Continue == "y":
-            #break
             correctUserInput = True
         else:
             print("wrong input")
